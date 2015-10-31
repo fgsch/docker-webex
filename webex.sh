bc1c2b107b162a5431b5ad9866b71b3723f29d2b
Federico G. Schwindt <fgsch@lodoss.net>
2015-10-31 02:27:45 +0000

    Simplify

--- webex.sh
+++ webex.sh
@@ -3,15 +3,9 @@
 docker rm --force webex || true
 docker build --tag=webex .
 docker run -ti \
-	--env DISPLAY \
-	--name=webex \
-	--net=host \
+	--env DISPLAY=unix$DISPLAY \
 	--lxc-conf='lxc.cgroup.devices.allow = c 116:* rwm' \
+	--name=webex \
 	--volume /dev/snd:/dev/snd \
-	--volume /dev/shm:/dev/shm \
-	--volume /etc/machine-id:/etc/machine-id \
-	--volume /run/user/$(id -u)/pulse:/run/user/webex/pulse \
-	--volume /var/lib/dbus:/var/lib/dbus \
-	--volume $HOME/.pulse:/home/webex/.pulse \
-	--volume $HOME/.Xauthority:/home/webex/.Xauthority \
+	--volume /tmp/.X11-unix:/tmp/.X11-unix \
 	webex
